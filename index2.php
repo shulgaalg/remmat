61e61dc9f5603441e805f0731bf8f11436eec974
Alexandr <shulga.alg@gmail.com>
2023-02-24 11:17:44 +0200

    24-02 11:33

--- index2.php
+++ index2.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="/dist/output.css" rel="stylesheet">
 
-
+Тест Марина
 
   <!-- <script src="https://cdn.tailwindcss.com"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css"-->
